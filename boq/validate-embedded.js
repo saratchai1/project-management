1efff1188b29aaef45fbe07b313974e03a85e245
saratchai1 <saratchai@gmail.com>
2026-09-07 13:36:57 +0700

    Assemble latest external65 master before BOQ validation

--- boq/validate-embedded.js
+++ boq/validate-embedded.js
@@ -2,6 +2,8 @@ const fs=require('fs');
 const cp=require('child_process');
 const parts=[1,2,3,4].map(i=>fs.readFileSync(`boq/embedded-snapshot-${String(i).padStart(2,'0')}.b64`,'utf8').trim());
 fs.writeFileSync('boq/embedded-snapshot.b64',parts.join(''));
+const external65Parts=[1,2,3,4].map(i=>fs.readFileSync(`boq/external65-master-20260907-${String(i).padStart(2,'0')}.b64`,'utf8').trim());
+fs.writeFileSync('boq/external65-master-20260907.b64',external65Parts.join(''));
 cp.execFileSync(process.execPath,['boq/validate.js'],{stdio:'inherit'});
 cp.execFileSync(process.execPath,['boq/extend-external66.js'],{stdio:'inherit'});
 const patchParts=[1,2,3,4,5].map(i=>fs.readFileSync(`boq/community-contract-view.patch.part${i}`,'utf8'));
