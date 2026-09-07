1731b0aaf4e6851cb0126a5ddd4eb1e94e093cd0
saratchai1 <saratchai@gmail.com>
2026-09-07 11:48:03 +0700

    Run external66 BOQ extension after embedded finance validation

--- boq/validate-embedded.js
+++ boq/validate-embedded.js
@@ -1,4 +1,6 @@
 const fs=require('fs');
+const cp=require('child_process');
 const parts=[1,2,3,4].map(i=>fs.readFileSync(`boq/embedded-snapshot-${String(i).padStart(2,'0')}.b64`,'utf8').trim());
 fs.writeFileSync('boq/embedded-snapshot.b64',parts.join(''));
-require('./validate.js');
+cp.execFileSync(process.execPath,['boq/validate.js'],{stdio:'inherit'});
+cp.execFileSync(process.execPath,['boq/extend-external66.js'],{stdio:'inherit'});
